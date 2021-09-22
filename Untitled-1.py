c33df0f27a035d58d76fc9aee615f924d7a30bb6
KubaS17 <90380998+KubaS17@users.noreply.github.com>
2021-09-22 12:07:55 +0200

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,7 @@
+print("programujemy \n w Pythonie")
+print("i nie tylko")
+print("to jest dodawanie", 23+5)
+a = int(input('Podaj pierwszą liczbę\n'))
+b = int(input('Podaj drugą liczbę\n'))
+print("Twoj wynik:", a*b)
+input("\n\n Aby zakonczyc, nacisnij ENTER")
---
+++ sałatk.py
@@ -0,0 +1,11 @@
+print('Robimy sałatke \n')
+print('dla jednej osoby potrzebujemy: 1 pomidor, 0.5 ogórka, 1 papryka')
+z1=1
+z2=0.5
+z3=1
+s2 = int(input('Podaj liczbę osób:\n'))
+s1=z1+z2+z3
+
+print('Będziesz potrzebował:', s1*s2, ('skladnikow'))
+input("\n\n Aby zakonczyc, nacisnij ENTER")
+
